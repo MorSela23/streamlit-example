c14c240a611ec40893dec7d8aed4b45954377ed8
Mor Sela <102901535+MorSela23@users.noreply.github.com>
2023-06-24 19:01:51 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,7 +49,7 @@ def bar_chart(df):
                                         text=genre_counts['duration'].round(2), textposition='auto',
                                         hovertemplate='Duration: %{text}')])
     
-    genre_chart.update_layout(title="Genre Distribution",
+    genre_chart.update_layout(
                               xaxis=dict(title="Count"),
                               yaxis=dict(title="Genre"),
                               bargap=0.2)
@@ -217,7 +217,7 @@ col1, col2, col3 = st.columns(3)
 with col2:
     title_photo = "netflix_logo.jpg"
     st.image(title_photo, use_column_width=False, width=400)
-    st.subheader("Analyzing the most popular movies / TV Shows dashboard")
+    st.subheader("Analyzing the most popular content dashboard")
     
 col4, col5 = st.columns(2, gap="large")
 with col4:
